5ec91869ca95ca014125a2d58421eea8ad15def1
radoslawkoziol <radoslaw.koziol@polcode.net>
2016-01-15 11:33:05 +0100

    default controller

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'TimeManager\Controller\TimeManager',
                         'action'     => 'index',
                     ),
                 ),
